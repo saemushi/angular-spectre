839301ddbc19a2da44ec0a90e8345a375e2ef7af
Rakesh <rakesh@mypass.com.au>
2018-08-07 11:41:53 +0530

    Initial commit

---
+++ projects/angular-spectre/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-spectre
+ */
+
+export * from './lib/angular-spectre.service';
+export * from './lib/angular-spectre.component';
+export * from './lib/angular-spectre.module';
