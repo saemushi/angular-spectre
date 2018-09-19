6eafd10d5d2f47e982529031dd2ec3acefbea28c
rrekala <41632756+rrekala@users.noreply.github.com>
2018-09-19 11:05:03 +0530

    Badge (#4)
    * initial commit
    
    * Initial commit
    
    * Added Pagination component
    
    * Added basic travis ci
    
    * Added badge directive. Scss compile setup in Lib
    
    * Lint fix

--- projects/angular-spectre/src/public_api.ts
+++ projects/angular-spectre/src/public_api.ts
@@ -5,3 +5,5 @@
 export * from './lib/pagination/pagination.service';
 export * from './lib/pagination/pagination.component';
 export * from './lib/pagination/pagination.module';
+
+export * from './lib/badge/badge.directive';
